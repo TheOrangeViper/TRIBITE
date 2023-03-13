c7b0c1f4a9c4eaf353160286e096df69b84b661c
KetingTrinh <trinhketing@gmail.com>
2023-03-13 15:48:21 -0400

    Added vertical and horizontal scrolling to home page
    There are now individual sections for specific recipes with horizontal scroll. These sections are stacked vertically. "Placeholders" will be made into buttons that redirect user to a recipe page.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,8 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, SafeAreaView, ScrollView, StatusBar} from 'react-native'
 import React from 'react'
 import {auth} from '../firebase'
 import { useNavigation } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-const Tab = createBottomTabNavigator();
 
 const HomeScreen = () => {
   
@@ -15,17 +13,76 @@ const HomeScreen = () => {
   // }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.header1}>Home</Text>
-      <Text>Email: {auth.currentUser?.email}</Text>
-      <Text>Welcome {auth.currentUser?.displayName}!</Text>
-      
-      {/* <TouchableOpacity
-        onPress={()=>{}}
-        style={styles.button}>
-        <Text style = {styles.buttonText}>Settings</Text>
-      </TouchableOpacity> */}
-    </View>
+    <SafeAreaView style={styles.container}>
+    <ScrollView 
+      style={styles.scrollView}
+      showsVerticalScrollIndicator={false}
+      >
+        <Text style={styles.header1}>Home</Text>
+        <TextInput
+          placeholder={"Search"}
+          style={styles.searchBar}
+          >
+        </TextInput>
+        {/* <Text>Email: {auth.currentUser?.email}</Text>
+        <Text>Welcome {auth.currentUser?.displayName}!</Text> */}
+
+        <Text style={styles.header2}>Meals</Text>
+        <ScrollView
+          horizontal={true}
+          style={styles.horizontalScroll}>
+
+          <Text style={styles.text}>
+          Meal placeholder</Text>
+          <Text style={styles.text}>
+          Meal placeholder</Text>
+          <Text style={styles.text}>
+          Meal placeholder</Text>
+          <Text style={styles.text}>
+          Meal placeholder</Text>
+          <Text style={styles.text}>
+          Meal placeholder</Text>
+          <Text style={styles.text}>
+          Meal placeholder</Text>
+        </ScrollView>
+        <Text style={styles.header2}>Snacks</Text>
+        <ScrollView
+          horizontal={true}
+          style={styles.horizontalScroll}>
+
+          <Text style={styles.text}>
+          Snack placeholder</Text>
+          <Text style={styles.text}>
+          Snack placeholder</Text>
+          <Text style={styles.text}>
+          Snack placeholder</Text>
+          <Text style={styles.text}>
+          Snack placeholder</Text>
+          <Text style={styles.text}>
+          Snack placeholder</Text>
+          <Text style={styles.text}>
+          Snack placeholder</Text>
+        </ScrollView>
+        <Text style={styles.header2}>Drinks</Text>
+        <ScrollView
+          horizontal={true}
+          style={styles.horizontalScroll}>
+
+          <Text style={styles.text}>
+          Drink placeholder</Text>
+          <Text style={styles.text}>
+          Drink placeholder</Text>
+          <Text style={styles.text}>
+          Drink placeholder</Text>
+          <Text style={styles.text}>
+          Drink placeholder</Text>
+          <Text style={styles.text}>
+          Drink placeholder</Text>
+          <Text style={styles.text}>
+          Drink placeholder</Text>
+        </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
@@ -34,12 +91,42 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    marginTop:'20%',
-    marginHorizontal:'10%',
+    marginBottom:'20%',
+  },
+  scrollView:{
+    // marginTop:'20%',
+    // marginHorizontal:'10%',
+    // marginBottom:'10%',
+  },
+  horizontalScroll:{
+    // backgroundColor:'red',
+    paddingLeft:'10%',
+    width:'100%'
+    
   },
   header1: {
     fontSize:50,
     fontWeight: 'bold',
+    marginTop:'20%',
+    marginHorizontal:'10%',
+    marginBottom:5,
+  },
+  header2: {
+    fontSize:25,
+    fontWeight: 'bold',
+    marginTop:'20%',
+    marginHorizontal:'10%',
+    marginBottom:5,
+  },
+  text:{
+    paddingHorizontal: 100,
+    paddingVertical:150,
+    alignItems:'center',
+    borderWidth:2,
+    borderRadius:20,
+    marginRight:30,
+    marginVertical:30,
+    backgroundColor:'white',
   },
   button:{
     backgroundColor: '#041e42',
@@ -59,5 +146,14 @@ const styles = StyleSheet.create({
     color:'white',
     fontWeight:'700',
     fontSize:16,
+  },
+  searchBar:{
+    marginVertical:10,
+    borderWidth:2,
+    borderRadius:20,
+    paddingHorizontal:20,
+    paddingVertical:5,
+    marginHorizontal:'10%',
+    // borderColor: '#d1d1d1',
   },
 })
