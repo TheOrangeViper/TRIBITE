6a42c2e17deebdd329eb0a9b023a31401fa07ed3
KetingTrinh <trinhketing@gmail.com>
2023-04-17 16:47:58 -0400

    Saved screen
    :skull: probably very broken

--- screens/Recipe.jsx
+++ screens/Recipe.jsx
@@ -8,16 +8,24 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 let testing = require("../screens/Testing");
 let savedRecipes = [];
 const user = auth.currentUser;
-let backcolour = "";
-let iconName = "";
+let backcolour = ""
+let iconName = ""
+// const [isScreenChange, setScreenChange] = useState()
 
 const Recipe = (props) => {
-  const { name, photoUrl, self } = props;
+  const {name, photoUrl, screenId} = props;
+  // make it so that for components on the saved will not show and auto hidden when there
+  
+  const navigation = useNavigation()
+  const [isSaved, setIsSaved] = useState(savedRecipes.indexOf(name)>-1)
 
-  const navigation = useNavigation();
-  const [isSaved, setIsSaved] = useState(savedRecipes.indexOf(name) > -1);
+  let saveState = ""
 
-  let saveState = "";
+  // useFocusEffect(
+  //   useCallback(() => {
+  //     console.log(screenId + " is active")
+  //   })
+  // )
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -48,37 +56,17 @@ const Recipe = (props) => {
     return unsubscribe;
   }, []);
 
-  function updateSavedStatus() {
-    let index = savedRecipes.indexOf(name);
-    const savedRef = ref(getDatabase(), "users/" + user.uid + "/savedRecipes");
-    onValue(
-      savedRef,
-      (snapshot) => {
-        snapshot.forEach((childSnapshot) => {
-          savedRecipes = snapshot.val();
-          if (index > -1) {
-            // is in saved recipes
-            setIsSaved(true);
-          } else {
-            // is not in saved recipes
-            setIsSaved(false);
-          }
-        });
-      },
-      { onlyOnce: true }
-    );
+  if(isSaved){
+    backcolour='yellow'
+    saveState = "Saved"
+    iconName='bookmark'
   }
-
-  if (isSaved) {
-    backcolour = "#FF4141";
-    saveState = "Saved";
-    iconName = "bookmark";
-  } else {
-    backcolour = "white";
-    saveState = "Not Saved";
-    iconName = "bookmark-outline";
+  else{
+    backcolour='white'
+    saveState = "Not Saved"
+    iconName='bookmark-outline'
   }
-
+    
   const redirect = () => {
     for (i = 0; i < testing.recipes.length; i++) {
       if (testing.recipes[i].name === name) {
@@ -88,51 +76,61 @@ const Recipe = (props) => {
     navigation.replace("Recipe");
   };
 
-  const testButton = () => {
-    updateSavedStatus();
-  };
-
-  const saveToDatabase = () => {
-    if (isSaved) {
-      // recipe in saved; want to unsave
-      savedRecipes.splice(savedRecipes.indexOf(name), 1);
-      setIsSaved(false);
-    } else {
-      // recipe is unsaved; want to save
-      savedRecipes.push(name);
-      setIsSaved(true);
+   const saveToDatabase = () => {   
+      // console.log(screenId)
+      if (isSaved) {
+        // recipe in saved; want to unsave  
+        console.log("UNSAVED")      
+        savedRecipes.splice(savedRecipes.indexOf(name),1)
+        if(screenId === "saved"){
+          console.log("unsaved from saved")
+          setIsHidden(true)
+        }   
+        setIsSaved(false)
+      }
+      else{
+        // recipe is unsaved; want to save
+        savedRecipes.push(name)     
+        setIsSaved(true)
+      }
+    set(ref(getDatabase(), 'users/' + auth.currentUser.uid), {
+      email : auth.currentUser.email,
+      savedRecipes : savedRecipes
+    })
+  }
+  
+  const [isHidden, setIsHidden] = useState(false);
+  const test = () => {
+    if(screenId === 'saved'){
+      setIsHidden(true)
     }
-    set(ref(getDatabase(), "users/" + auth.currentUser.uid), {
-      email: auth.currentUser.email,
-      savedRecipes: savedRecipes,
-    });
-  };
+  }
 
-  return (
-    <TouchableOpacity
-      key={name}
-      style={styles.tileContainer}
-      onPress={() => {
-        redirect();
-      }}
-    >
-      <View style={styles.imageContainer}>
-        <Image source={{ uri: photoUrl }} style={styles.image} />
-        <TouchableOpacity
-          style={{
-            height: 75,
-            width: 75,
-            position: "absolute",
-            alignSelf: "flex-end",
-          }}
-          onPress={() => saveToDatabase()}
-        >
-          <Ionicons name={iconName} size={60} color={backcolour} />
-        </TouchableOpacity>
-      </View>
-      <Text style={styles.itemText}> {name} </Text>
-    </TouchableOpacity>
-  );
+  if(screenId === 'saved'){
+    testColor = 'pink'
+  }
+  else {
+    testColor = "orange"
+  }
+  
+  if(isHidden){
+    return;
+  }
+    else{
+      return(
+        <TouchableOpacity key = {name} style = {styles.tileContainer} onLongPress = { () => {console.log(name, " was pressed"); redirect()}} >
+          <View style = {styles.imageContainer}>
+            <Image source={{uri: photoUrl}} style={styles.image}/>
+            <TouchableOpacity style = {{height:75, width:75, position:'absolute', alignSelf: 'flex-end'}} onPress={() => saveToDatabase()}>
+              <Ionicons name={iconName} size={60} color={backcolour}/>
+            </TouchableOpacity>
+          </View>
+          <Text style = {styles.othertext}> {name} </Text>
+          {/* <TouchableOpacity style = {{backgroundColor: testColor}} onPress={() => test()}>
+            <Text>We at this again</Text>
+          </TouchableOpacity> */}
+        </TouchableOpacity>);
+    }
 };
 
 export default Recipe;
--- screens/SavedScreen.js
+++ screens/SavedScreen.js
@@ -1,12 +1,64 @@
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, SafeAreaView, ScrollView, Image} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {getDatabase, ref, set, onValue} from "firebase/database";
+import { auth } from '../firebase';
+import Recipe from './Recipe';
+
+
+let db = require('../screens/Testing')
 
 
 const SavedScreen = () => {
+  // uh make is so that it filters from the recipe.jsxs
+  
+  const [save, setSave] = useState()
+  let savedRecipes = []
+  let savedNames = []
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        const savedRef = ref(getDatabase(), 'users/' + user.uid + '/savedRecipes');
+        onValue(savedRef, (snapshot) => {
+          snapshot.forEach((childSnapshot) => {
+            savedNames = snapshot.val(); 
+            for (i = 0; i < savedNames.length; i++){
+              // For each saved recipe
+              for (j = 0; j < db.recipes.length; j++){
+                // Add the object to list
+                if(db.recipes[j].name === savedNames[i]){
+                  // console.log(db.recipes[i].name + " is saved")
+                  // A recipe is a saved recipe, add it to the list
+                  if(savedRecipes.indexOf(db.recipes[j])===-1){
+                    savedRecipes.push(db.recipes[j])
+                  }else{
+                  }
+                }
+              }
+            }
+            setSave(savedRecipes)
+            // console.log(save)           
+          });
+        }
+        // , {onlyOnce : true}
+        );
+        // console.log(savedRecipes[0])
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  // console.log(save)
   return (
-    <KeyboardAvoidingView style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.header1}>Saved</Text>
-    </KeyboardAvoidingView>
+      <ScrollView>
+        <View style = {styles.itemContainer}>
+          {save.map((recipe)=>{
+              return(<Recipe key={recipe.name} name={recipe.name} photoUrl={recipe.photoUrl} recipe={recipe} screenId={"saved"}/>);
+              })}
+        </View>
+      </ScrollView>
+    </View>
   )
 }
 
@@ -17,6 +69,9 @@ const styles = StyleSheet.create({
     flex:1,
     marginTop:'20%',
     marginHorizontal:'10%',
+  },
+  itemContainer:{
+    alignItems:'center'
   },
   header1: {
     fontSize:50,
